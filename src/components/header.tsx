252b9091968f6fc23d8ba0508a87b1358220ae98
benjichat <63160791+benjichat@users.noreply.github.com>
2023-04-20 08:10:24 +0200

    Adds Metrics component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
 
 import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
 
 export const Header = () => {
   const { data: sessionData } = useSession();
@@ -8,6 +9,21 @@ export const Header = () => {
     <div className="navbar bg-primary text-primary-content">
       <div className="flex-1 pl-5 text-3xl font-bold">
         {sessionData?.user?.name ? `Notes for ${sessionData.user.name}` : ""}
+      </div>
+      <div className="flex-1 pl-5 text-3xl font-bold">
+        <Link href="/metrics">
+          My Metrics
+        </Link>
+      </div>
+      <div className="flex-1 pl-5 text-3xl font-bold">
+        <Link href="/report">
+          Report
+        </Link>
+      </div>
+      <div className="flex-1 pl-5 text-3xl font-bold">
+        <Link href="/view">
+          View
+        </Link>
       </div>
       <div className="flex-none gap-2">
         <div className="dropdown-end dropdown">
---
+++ src/components/metricsForm.tsx
@@ -0,0 +1,86 @@
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+import { api, type RouterOutputs } from "../utils/api";
+
+
+interface Props {
+  // Define any props needed for your component
+}
+
+type Metric = RouterOutputs["metrics"]["getAll"][0];
+
+export const MetricsForm = () => {
+
+  const { data: sessionData } = useSession();
+
+  // const [selectedTopic, setSelectedTopic] = useState<Metric | null>(null);
+
+  const [newMetric, setNewMetric] = useState<Metric | null>(null);
+
+  const { data: metrics, refetch: refetchTopics } = api.metrics.getAll.useQuery(
+    undefined, // no input
+    {
+      enabled: sessionData?.user !== undefined,
+      // onSuccess: (data) => {
+      //   setSelectedTopic(selectedTopic ?? data[0] ?? null);
+      // },
+    }
+  );
+
+  const createMetric = api.metrics.create.useMutation({
+    onSuccess: () => {
+      void refetchTopics();
+    },
+  });
+
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <div className="w-full max-w-xs bg-white shadow-md p-6 rounded-lg">
+        <table className="w-full">
+          {/* Table header */}
+          <thead>
+            <tr>
+              {/* Add table headers as needed */}
+              <th className="w-1/4 px-4 py-3 text-left">#</th>
+              <th className="w-3/4 px-4 py-3">Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {metrics?.map((metric, index) => (
+              <tr key={metric.id}>
+                <td className="w-1/4 px-4 py-3 text-left">{index}</td>
+                <td className="w-3/4 px-4 py-3 text-center">{metric.name}</td>
+              </tr>
+            ))}
+            <tr>
+              <td colSpan={3} className="p-4">
+                <form className="space-y-4">
+                  <input
+                    type="text"
+                    placeholder="New Topic"
+                    className="input-bordered input input-sm w-full"
+                    onChange={(e) => {
+                      setNewMetric(e.currentTarget.value);
+                    }}
+                  />
+                  <button
+                    type="submit"
+                    className="w-full px-4 py-2 bg-blue-600 text-white font-semibold rounded"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      createMetric.mutate({
+                        name: newMetric,
+                      });
+                    }}
+                  >
+                    Add Metric
+                  </button>
+                </form>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
